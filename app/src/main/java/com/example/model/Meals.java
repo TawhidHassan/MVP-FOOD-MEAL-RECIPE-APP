4fe4ee9b8f5e325d02f1e521f54dfcf833146da1
sifat hassan <sifat27.sh22@gmail.com>
2020-06-21 12:32:06 +0600

    ready template with mvp

---
+++ app/src/main/java/com/example/model/Meals.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+public class Meals {
+
+    /*
+     * TODO 8 Paste the generated results
+     *
+     * Paste the generated results from -
+     * http://www.jsonschema2pojo.org/
+     */
+
+
+    public class Meal {
+        /*
+         * TODO 9 Paste the generated results
+         *
+         * Paste the generated results Level 2 (Meal Item) from -
+         * http://www.jsonschema2pojo.org/
+         */
+    }
+}
---
+++ app/src/main/java/com/example/view.home/HomeActivity.java
@@ -0,0 +1,41 @@
+package com.example.view.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mvpfoodrecipeapp.R;
+
+
+// TODO 31 implement the HomeView interface at the end
+public class HomeActivity extends AppCompatActivity {
+
+    /*
+     * TODO 32 Add @BindView Annotation (1)
+     *
+     * 1. viewPagerHeader
+     * 2. recyclerCategory
+     *
+     */
+
+    /*
+     *  TODO 33 Create variable for presenter;
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        /*
+         *  TODO 34 bind the ButterKnife (2)
+         */
+
+        /*
+         *  TODO 35 Declare the presenter
+         *  new HomePresenter(this)
+         */
+    }
+
+    // TODO 36 Overriding the interface
+
+}
+
